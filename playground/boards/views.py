ac348dd6b0ba269f958e6392a25a6259969fb048
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 10:22:05 +0100

    Attempt add new topic page

--- playground/boards/views.py
+++ playground/boards/views.py
@@ -10,6 +10,10 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
+
 def user(request, username):
     return render(request, 'user.html', {'username': username})
 
