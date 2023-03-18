f501f7578f6ff5b34f70147b9f9e97772d7ccd63
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 10:48:35 +0100

    Add tests for new topic following instructions

--- playground/boards/tests.py
+++ playground/boards/tests.py
@@ -53,7 +53,7 @@ class BoardTopicsTests(TestCase):
 
 class NewTopicTests(TestCase):
     def setUp(self):
-        self.board = Board.objects.create(name='Django', description='Django board.')
+        Board.objects.create(name='Django', description='Django board.')
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -70,8 +70,8 @@ class NewTopicTests(TestCase):
         self.assertEquals(view.func, new_topic)
 
     def test_new_topic_view_contains_link_back_to_board_topics_view(self):
-        url = reverse('new_topic', kwargs={'pk': 1})
-        response = self.client.get(url)
-        import ipdb; ipdb.set_trace()
-        self.assertContains(response, 'href=/boards/1/')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(new_topic_url)
+        self.assertContains(response, f'href={board_topics_url}')
 
