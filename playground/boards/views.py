f566ea2bbbf8f605f9daf3b9ca94efb0ff8f5676
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 22:11:00 +0100

    Add login requirement for new topic

--- playground/boards/views.py
+++ playground/boards/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import NewTopicForm
@@ -12,6 +13,7 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first() #TODO: get the currently logged in user
