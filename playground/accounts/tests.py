f2305d18809c6795cc7bfb253391ec506c52dc6d
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 14:47:47 +0100

    Add user creation tests

--- playground/accounts/tests.py
+++ playground/accounts/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import resolve, reverse
+from django.contrib.auth.models import User
 from django.test import TestCase
 from .views import signup
+
 
 
 class SignUpTests(TestCase):
@@ -22,4 +24,35 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'example',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456',
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        '''
+        A valid form submisstion should redirect the user to the home page
+        '''
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        '''
+        Create a new request to an arbitrary page.
+        The resulting response should now have a `user` to its context,
+        after a successful sign up.
+        '''
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
 
