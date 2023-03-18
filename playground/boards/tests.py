455b5cc873e6fb56873a518362c40390462b961b
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 10:45:37 +0100

    Attempt add tests for new topic

--- playground/boards/tests.py
+++ playground/boards/tests.py
@@ -1,6 +1,6 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
-from .views import home, board_topics
+from .views import home, board_topics, new_topic
 from .models import Board
 
 # Create your tests here.
@@ -50,3 +50,28 @@ class BoardTopicsTests(TestCase):
             # Allow instances of missing quotes
             self.assertContains(response, 'href={0}'.format(homepage_url))
             
+
+class NewTopicTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+
+    def test_new_topic_view_success_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_view_not_found_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 999})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_new_topic_url_resolves_new_topic_view(self):
+        view = resolve('/boards/1/new/')
+        self.assertEquals(view.func, new_topic)
+
+    def test_new_topic_view_contains_link_back_to_board_topics_view(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        import ipdb; ipdb.set_trace()
+        self.assertContains(response, 'href=/boards/1/')
+
