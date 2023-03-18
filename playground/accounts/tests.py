e40ef6f5d4ea58c4d60d04330cc68e1cfd0ab083
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 15:05:44 +0100

    Add test checking for correct fields in login html

--- playground/accounts/tests.py
+++ playground/accounts/tests.py
@@ -26,6 +26,16 @@ class SignUpTests(TestCase):
         form = self.response.context.get('form')
         self.assertIsInstance(form, SignUpForm)
 
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
 
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
