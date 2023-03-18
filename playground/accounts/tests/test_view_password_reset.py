fefd2f2b5a5c8b36e0c577adb13b3e0e8547ee71
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 19:07:17 +0100

    Add page for resetting password with link

--- playground/accounts/tests/test_view_password_reset.py
+++ playground/accounts/tests/test_view_password_reset.py
@@ -66,3 +66,16 @@ class InvalidPasswordResetTests(TestCase):
     def test_no_email_sent(self):
         self.assertEquals(0, len(mail.outbox))
 
+
+class PasswordResetDoneTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_done')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/done/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
+
