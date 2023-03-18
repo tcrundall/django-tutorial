917c69883dcefd640a37454ccaf2c7d31dda0459
Timothy Crundall <tim.crundall@gmail.com>
2023-03-18 17:58:08 +0100

    Add urls for password resetting and empty template files

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -22,6 +22,30 @@ import accounts.views as accounts_views
 import boards.views as boards_views
 
 urlpatterns = [
+    re_path(
+        r'^reset/$',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt',
+        ),
+        name='password_reset',
+    ),
+    re_path(
+        r'^reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done',
+    ),
+    re_path(
+        r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm',
+    ),
+    re_path(
+        r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete',
+    ),
     re_path('^$', boards_views.home, name='home'),
     re_path('^signup/$', accounts_views.signup, name='signup'),
     re_path('^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
