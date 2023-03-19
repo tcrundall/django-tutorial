f369a56fb890bb9416410ce54d7ce9b12e4653fe
Timothy Crundall <tim.crundall@gmail.com>
2023-03-19 14:56:37 +0100

    Add actual counters and some links to home page

--- playground/boards/models.py
+++ playground/boards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 
 # Create your models here.
 class Board(models.Model):
@@ -9,11 +10,20 @@ class Board(models.Model):
     def __str__(self):
         return self.name
 
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, models.CASCADE, related_name='topics')
     starter = models.ForeignKey(User, models.CASCADE, related_name='topics')
+
+    def __str__(self):
+        return self.subject
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -22,4 +32,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, models.CASCADE, related_name='posts')
     updated_by = models.ForeignKey(User, models.CASCADE, null=True, related_name='+')
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
 
