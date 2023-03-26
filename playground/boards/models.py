e860c30df69911fcaa69680898b815b1d7e27fb6
Timothy Crundall <tim.crundall@gmail.com>
2023-03-26 19:29:49 +0200

    Use sessions to track topic views, add links to reply pages

--- playground/boards/models.py
+++ playground/boards/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils.html import mark_safe # type: ignore
 from django.utils.text import Truncator
 from markdown import markdown
+import math
 
 # Create your models here.
 class Board(models.Model):
@@ -27,6 +28,23 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()  # type: ignore
+        pages = count / 20
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)
+
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
